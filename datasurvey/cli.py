931734d906026db6d791bf375b2961debc4d9853
Smári McCarthy <smari@occrp.org>
2016-01-15 15:14:02 +0100

    Use CSV writer; nicer options structure

--- datasurvey/cli.py
+++ datasurvey/cli.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 import os
 import click
+import csv
 from magic import Magic
 
 class Scanner:
-    def __init__(self, mime=False):
-        self.magic = Magic(magic_file='magic.db', mime=mime, uncompress=True)
-        self.mime = mime
+    def __init__(self, **options):
+        self.options = options
+        self.mime = options['mime']
+        self.magic = Magic(magic_file='magic.db', mime=self.mime, uncompress=True)
         self._reset()
 
     def _reset(self):
         self.types = {}
+        self.files = {}
         self.total = 0
 
     def scan_file(self, file):
@@ -20,30 +23,42 @@ class Scanner:
         for root, directory, files in os.walk(path):
             for file in files:
                 self.total += 1
-                mime = self.scan_file(os.path.join(root, file))
+                path = os.path.join(root, file)
+                mime = self.scan_file(path)
                 if not mime in self.types: self.types[mime] = []
-                self.types[mime].append(file)
-
+                self.files[path] = {}
+                self.files[path]['mime'] = mime
+                self.types[mime].append(path)
 
 
 class Reporter:
-    def __init__(self, scanner):
+    def __init__(self, options, scanner):
+        self.options = options
         self.scanner = scanner
+        self.outfile = options['target']
 
     def report(self):
         raise NotImplemented()
 
+
 class ReportCSV(Reporter):
     def report(self):
-        print "\"filename\",\"%s\"" % (["type", "mimetype"][self.scanner.mime])
-        for t, fs in self.scanner.types.iteritems():
-            for f in fs:
-                print "\"%s\",\"%s\"" % (f, t)
+        writer = csv.writer(self.options['target'])
+
+        headers = ["filename", ["type", "mimetype"][self.scanner.mime]]
+
+        writer.writerow(headers)
+        for f, details in self.scanner.files.iteritems():
+            line = [f, details['mime']]
+            writer.writerow(line)
+
+
 
 class ReportAggregate(Reporter):
     def report(self):
         for t, fs in self.scanner.types.iteritems():
-            print "%-35s: %d (%d%%)" % (t, len(fs), len(fs)*100/self.scanner.total)
+            self.outfile.write("%-35s: %d (%d%%)\n" % (t, len(fs),
+                len(fs)*100/self.scanner.total))
 
 
 outputmodes = {
@@ -53,12 +68,14 @@ outputmodes = {
 
 @click.command()
 @click.argument('path', type=click.Path(exists=True))
-@click.option('--mime', is_flag=True)
-@click.option('--output', type=click.Choice(outputmodes.keys()), default="report")
-def main(path, mime, output):
-    s = Scanner(mime)
-    s.scan(path)
-    reporter = outputmodes[output](s)
+@click.option('--mime', is_flag=True, help="Show mime types")
+@click.option('--format', type=click.Choice(outputmodes.keys()),
+    default="report", help="Choose output format")
+@click.option('--target', type=click.File('w'), default='-')
+def main(path, **options):
+    scanner = Scanner(**options)
+    scanner.scan(path)
+    reporter = outputmodes[options['format']](options, scanner)
     reporter.report()
 
 
